bb519d0c38bc3546195927fbfbaaa55d263d1ae4
mlmargaretli6@gmail.com <>
2021-04-20 15:05:39 -0700

    x

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+  }
+
+  if (mouseIsPressed) {
+
+  }
+}
+
+function randomizer() {
+  animating = false;
+
+  }
+
+function buttonPressed() {
+  animating = true;
+  setTimeout(randomizer, 1000);
+}
+
+function keyTyped() {
+  if (key === 's') { //save image
+    saveCanvas('drawing', 'png');
+  }
+}
+
+function drawAnimal() {
+
+}
+
+function mousePressed() {
+
+}
+
+function drawPixels() {
+
+}
